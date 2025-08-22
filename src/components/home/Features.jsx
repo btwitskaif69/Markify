5870f80ba605c04e511b6c860b6a6e4f689b49e7
Mohd Kaif <124128116+btwitskaif69@users.noreply.github.com>
2025-08-22 22:05:38 +0530

    playing around with hover effect

--- src/components/home/Features.jsx
+++ src/components/home/Features.jsx
@@ -6,6 +6,7 @@ import {
   GlobeIcon,
   InputIcon,
 } from "@radix-ui/react-icons";
+import bg from '@/assets/global-search.png'
  
 import { BentoCard, BentoGrid } from "@/components/magicui/bento-grid";
  
@@ -16,7 +17,7 @@ const features = [
     description: "AI-powered categorization that learns from your browsing habits and automatically sorts bookmarks into intelligent collections.",
     href: "/",
     cta: "Learn more",
-    background: <img className="absolute -right-20 -top-20 opacity-60" />,
+    background: <img src={bg} className="absolute top-10 object-cover [--duration:20s] [mask-image:linear-gradient(to_top,transparent_40%,#000_100%)]" />,
     className: "lg:row-start-1 lg:row-end-4 lg:col-start-2 lg:col-end-3",
   },
   {
@@ -25,7 +26,8 @@ const features = [
     description: "Find any bookmark in milliseconds with our advanced CMD+K or CTRL+K search that understands context, tags, and content.",
     href: "/",
     cta: "Learn more",
-    background: <img className="absolute -right-20 -top-20 opacity-60" />,
+    background: <img src={bg} className="absolute scale-170 duration-300 brightness-150 contrast-125 hover:scale-180 ease-in-out [--duration:20s]
+                                        [mask-image:linear-gradient(to_top,transparent_10%,#000_100%)]" />,
     className: "lg:col-start-1 lg:col-end-2 lg:row-start-1 lg:row-end-3",
   },
   {
@@ -34,7 +36,7 @@ const features = [
     description: "Your bookmarks stay yours. End-to-end encryption ensures your browsing history remains completely private.",
     href: "/",
     cta: "Learn more",
-    background: <img className="absolute -right-20 -top-20 opacity-60" />,
+    background: <img src={bg} className="absolute -right-20 -top-20 opacity-60" />,
     className: "lg:col-start-1 lg:col-end-2 lg:row-start-3 lg:row-end-4",
   },
   {
@@ -43,7 +45,7 @@ const features = [
     description: "Access your bookmarks anywhere, anytime. Seamless synchronization across all your devices and browsers.",
     href: "/",
     cta: "Learn more",
-    background: <img className="absolute -right-20 -top-20 opacity-60" />,
+    background: <img src={bg} className="absolute -right-20 -top-20 opacity-60" />,
     className: "lg:col-start-3 lg:col-end-3 lg:row-start-1 lg:row-end-2",
   },
   {
@@ -53,14 +55,14 @@ const features = [
       "Share Bookmarks with your team and friends in one click",
     href: "/",
     cta: "Learn more",
-    background: <img className="absolute -right-20 -top-20 opacity-60" />,
+    background: <img src={bg} className="absolute -right-20 -top-20 opacity-60" />,
     className: "lg:col-start-3 lg:col-end-3 lg:row-start-2 lg:row-end-4",
   },
 ];
 
 const Features = () => {
   return (
-    <div className='p-5'>
+    <div className='py-20 px-4 sm:px-6 md:px-8 lg:px-30'>
        <BentoGrid className="lg:grid-rows-3">
       {features.map((feature) => (
         <BentoCard key={feature.name} {...feature} />
